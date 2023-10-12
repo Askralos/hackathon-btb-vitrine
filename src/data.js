870d4cf1e71c14aede2763e90658a2a34baa17b6
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-10-12 22:57:07 +0200

    correcting linksfor assets

--- src/data.js
+++ src/data.js
@@ -1,31 +1,30 @@
 export const team = [
-	{
-		people: "Gabby",
-		classe: "B3 Info",
-		role: "Back-end",
-	},
-	{
-		people: "Gaël",
-		classe: "M1 Info",
-		role: "Data-analyst",
-	},
   {
-		people: "Siweil",
-		classe: "B2 Info",
-		role: "Front-end, Designer",
-		img: "../src/assets/siweil.jpg",
-	},
-	{
-		people: "Moustapha",
-		classe: "M1 Info",
-		img: "../src/assets/mousse.jpg",
-		role: "Front-end",
-	},
-	{
-		people: "Alvine",
-		classe: "B2 Info",
-		img: "../src/assets/alvine.jpg",
-		role: "Font-end",
-	},
-
+    people: "Gabby",
+    classe: "B3 Infra",
+    role: "Back-end",
+  },
+  {
+    people: "Gaël",
+    classe: "B3 Dev Web",
+    role: "Data-analyst",
+  },
+  {
+    people: "Siweil",
+    classe: "B2 Info",
+    role: "Front-end, Designer",
+    img: "/assets/siweil.jpg",
+  },
+  {
+    people: "Moustapha",
+    classe: "B3 Dev Web",
+    img: "/assets/mousse.jpg",
+    role: "Front-end",
+  },
+  {
+    people: "Alvine",
+    classe: "B2 Info",
+    img: "/assets/alvine.jpg",
+    role: "Font-end",
+  },
 ];
