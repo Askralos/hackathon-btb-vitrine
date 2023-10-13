5a25d44cbf5ad083554b5cb2c0bbfe7cc892d94c
Askralos <moeun@pc-fixe-sm>
2023-10-13 22:57:02 +0200

    add: deploy workflows

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/hackathon-btb-vitrine',
+  base:'/hackathon-BTB-vitrine',
   plugins: [react()],
 })
