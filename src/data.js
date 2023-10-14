e84efafba929d406097d7a7ac4df497c535b40ac
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-10-14 14:36:31 +0200

    removing base so that all src links can emerge from correct source

--- src/data.js
+++ src/data.js
@@ -1,33 +1,32 @@
 export const team = [
-	{
-		people: "Gabby",
-		classe: "B3 Infra",
-		role: "Back-end",
-		img: "../src/assets/Gabby.jpg",
-	},
-	{
-		people: "Gaël",
-		classe: "B3 Dev",
-		role: "Back-end",
-		img: "../src/assets/gael.png",
-	},
   {
-		people: "Siweil",
-		classe: "B2 Info",
-		role: "Front-end, Designer",
-		img: "../src/assets/siweil.jpg",
-	},
-	{
-		people: "Moustapha",
-		classe: "B3 Dev",
-		img: "../src/assets/mousse.jpg",
-		role: "Front-end",
-	},
-	{
-		people: "Alvine",
-		classe: "B2 Info",
-		img: "../src/assets/alvine.jpg",
-		role: "Font-end",
-	},
-
+    people: "Gabby",
+    classe: "B3 Infra",
+    role: "Back-end",
+    img: "../assets/Gabby.jpg",
+  },
+  {
+    people: "Gaël",
+    classe: "B3 Dev",
+    role: "Back-end",
+    img: "../assets/gael.png",
+  },
+  {
+    people: "Siweil",
+    classe: "B2 Info",
+    role: "Front-end, Designer",
+    img: "../assets/siweil.jpg",
+  },
+  {
+    people: "Moustapha",
+    classe: "B3 Dev",
+    img: "../assets/mousse.jpg",
+    role: "Front-end",
+  },
+  {
+    people: "Alvine",
+    classe: "B2 Info",
+    img: "../assets/alvine.jpg",
+    role: "Font-end",
+  },
 ];
